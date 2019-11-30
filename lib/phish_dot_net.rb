be9ed1b8426291b8ba562632c381f3c24df5a7e3
PlanetEfficacy <jspevack@gmail.com>
2019-11-29 19:09:44 -0700

    first commit

---
+++ lib/phish_dot_net.rb
@@ -0,0 +1,6 @@
+require "phish_dot_net/version"
+
+module PhishDotNet
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/phish_dot_net/version.rb
@@ -0,0 +1,3 @@
+module PhishDotNet
+  VERSION = "0.1.0"
+end
